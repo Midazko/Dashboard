32fe1125872d2b6df0af7642ed7725e605541022
CarlAxelAven <lexalrac@gmail.com>
2022-12-27 11:33:19 +0100

    asd

--- Dashboard/frmAnalytics.Designer.cs
+++ Dashboard/frmAnalytics.Designer.cs
@@ -29,26 +29,27 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAnalytics));
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.LightSalmon;
-            this.label1.Location = new System.Drawing.Point(519, 150);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Your Music Here";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(536, 189);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(177, 103);
@@ -56,27 +57,156 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(80, 161);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1086, 338);
+            this.panel1.TabIndex = 2;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(974, 88);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(79, 231);
+            this.button9.TabIndex = 12;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(868, 88);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(79, 231);
+            this.button10.TabIndex = 11;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(765, 88);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(79, 231);
+            this.button7.TabIndex = 10;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(659, 88);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(79, 231);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(557, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 231);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(451, 88);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 231);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 231);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 88);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 231);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 231);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 231);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(185, 14);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(214, 55);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Stop";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
             // frmAnalytics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1253, 653);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmAnalytics";
             this.Text = "frmAnalytics";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Label label1;
         private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button9;
+        private Button button10;
+        private Button button7;
+        private Button button8;
+        private Button button5;
+        private Button button6;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+        private Button button1;
+        private Button button11;
     }
 }
--- Dashboard/frmAnalytics.cs
+++ Dashboard/frmAnalytics.cs
@@ -7,14 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using System.Numerics;
 
 namespace Dashboard
 {
     public partial class frmAnalytics : Form
     {
+        SoundPlayer player1 = new SoundPlayer(@"E:\git\Dashboard\audio\Losing_My_Edge[1].wav");
+        SoundPlayer player2 = new SoundPlayer(@"E:\git\Dashboard\audio\Black_Sabbath_-_Sabotage_-_04_-_Megalomania[1].wav");
+        SoundPlayer player3 = new SoundPlayer(@"E:\git\Dashboard\audio\Jethro_Tull_-_Aqualung_-_40th_Anniversary_Special_Edition_-_CD1_-_10_-_Locomotive_Breath[1].wav");
+        SoundPlayer player4 = new SoundPlayer(@"E:\git\Dashboard\audio\Jethro_Tull_-_Thick_As_A_Brick_-_01_-_Thick_As_A_Brick_(part_1)[1].wav");
+        SoundPlayer player5 = new SoundPlayer(@"E:\git\Dashboard\audio\Jethro_Tull_-_Thick_As_A_Brick_-_02_-_Thick_As_A_Brick_(part_2)[1].wav");
+        SoundPlayer player6 = new SoundPlayer(@"E:\git\Dashboard\audio\Yes_-_Close_To_The_Edge_(2013_Steven_Wilson_Remix)_-_01_-_Close_To_The_Edge[1].wav");
+        SoundPlayer player7 = new SoundPlayer(@"E:\git\Dashboard\audio\Yes_-_Close_To_The_Edge_(2013_Steven_Wilson_Remix)_-_02_-_And_You_And_I[1].wav");
+        SoundPlayer player8 = new SoundPlayer(@"E:\git\Dashboard\audio\Yes_-_Close_To_The_Edge_(2013_Steven_Wilson_Remix)_-_03_-_Siberian_Khatru[1].wav");
+        SoundPlayer player9 = new SoundPlayer(@"E:\git\Dashboard\audio\Yes_-_Fragile_-_09_-_Heart_of_the_Sunrise[1].wav");
+        SoundPlayer player10 = new SoundPlayer(@"E:\git\Dashboard\audio\Yes_-_Fragile_-_16_-_America_(Singel_1972)[1].wav");
         public frmAnalytics()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            player1.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player2.Play();        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            player3.Play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            player4.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            player5.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            player6.Play();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            player7.Play();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            player8.Play();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            player9.Play();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            player10.Play();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            player1.Play();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            player1.Stop();
+            player2.Stop();
+            player3.Stop();
+            player4.Stop();
+            player5.Stop();
+            player6.Stop();
+            player7.Stop();
+            player8.Stop();
+            player9.Stop();
+            player10.Stop();
         }
     }
 }
