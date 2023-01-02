f161d8c206cb72c4ed7a78fd551daa95287125b9
Jerpster <jesda1111@hotmail.com>
2023-01-02 10:04:08 +0100

    Update frmJesper.Designer.cs
    fixat font och lite annat i Saved Notes grid

--- Dashboard/frmJesper.Designer.cs
+++ Dashboard/frmJesper.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.TitleBox = new System.Windows.Forms.TextBox();
             this.NotesBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.Notes = new System.Windows.Forms.Label();
             this.PreviousNotes = new System.Windows.Forms.DataGridView();
             this.EditButton = new System.Windows.Forms.Button();
             this.DeleteButton = new System.Windows.Forms.Button();
             this.NewNoteButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
+            this.SavedNotes = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PreviousNotes)).BeginInit();
             this.SuspendLayout();
             // 
             // TitleBox
             // 
+            this.TitleBox.ForeColor = System.Drawing.Color.LightSalmon;
             this.TitleBox.Location = new System.Drawing.Point(745, 113);
             this.TitleBox.Name = "TitleBox";
             this.TitleBox.Size = new System.Drawing.Size(496, 23);
@@ -49,39 +52,51 @@
             // 
             // NotesBox
             // 
+            this.NotesBox.ForeColor = System.Drawing.Color.LightSalmon;
             this.NotesBox.Location = new System.Drawing.Point(745, 182);
             this.NotesBox.Multiline = true;
             this.NotesBox.Name = "NotesBox";
             this.NotesBox.Size = new System.Drawing.Size(496, 459);
             this.NotesBox.TabIndex = 1;
             // 
-            // label1
+            // Title
             // 
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.LightSalmon;
-            this.label1.Location = new System.Drawing.Point(745, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(496, 28);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Title:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Title.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Title.ForeColor = System.Drawing.Color.LightSalmon;
+            this.Title.Location = new System.Drawing.Point(745, 82);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(496, 28);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Title:";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label2
+            // Notes
             // 
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.LightSalmon;
-            this.label2.Location = new System.Drawing.Point(745, 151);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(496, 28);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Notes:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Notes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Notes.ForeColor = System.Drawing.Color.LightSalmon;
+            this.Notes.Location = new System.Drawing.Point(745, 151);
+            this.Notes.Name = "Notes";
+            this.Notes.Size = new System.Drawing.Size(496, 28);
+            this.Notes.TabIndex = 3;
+            this.Notes.Text = "Notes:";
+            this.Notes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // PreviousNotes
             // 
+            this.PreviousNotes.DefaultCellStyle.Font = new Font("Arial", 12F);
+            this.PreviousNotes.ForeColor = Color.LightSalmon;
             this.PreviousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.PreviousNotes.BackgroundColor = System.Drawing.Color.White;
             this.PreviousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.LightSalmon;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.PreviousNotes.DefaultCellStyle = dataGridViewCellStyle1;
+            this.PreviousNotes.GridColor = System.Drawing.Color.Black;
             this.PreviousNotes.Location = new System.Drawing.Point(12, 182);
             this.PreviousNotes.Name = "PreviousNotes";
             this.PreviousNotes.RowTemplate.Height = 25;
@@ -94,7 +109,7 @@
             this.EditButton.BackColor = System.Drawing.Color.White;
             this.EditButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.EditButton.ForeColor = System.Drawing.Color.LightSalmon;
-            this.EditButton.Location = new System.Drawing.Point(481, 538);
+            this.EditButton.Location = new System.Drawing.Point(493, 538);
             this.EditButton.Name = "EditButton";
             this.EditButton.Size = new System.Drawing.Size(115, 46);
             this.EditButton.TabIndex = 5;
@@ -107,7 +122,7 @@
             this.DeleteButton.BackColor = System.Drawing.Color.White;
             this.DeleteButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.DeleteButton.ForeColor = System.Drawing.Color.LightSalmon;
-            this.DeleteButton.Location = new System.Drawing.Point(602, 538);
+            this.DeleteButton.Location = new System.Drawing.Point(614, 538);
             this.DeleteButton.Name = "DeleteButton";
             this.DeleteButton.Size = new System.Drawing.Size(115, 46);
             this.DeleteButton.TabIndex = 6;
@@ -120,7 +135,7 @@
             this.NewNoteButton.BackColor = System.Drawing.Color.White;
             this.NewNoteButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.NewNoteButton.ForeColor = System.Drawing.Color.LightSalmon;
-            this.NewNoteButton.Location = new System.Drawing.Point(602, 590);
+            this.NewNoteButton.Location = new System.Drawing.Point(614, 590);
             this.NewNoteButton.Name = "NewNoteButton";
             this.NewNoteButton.Size = new System.Drawing.Size(115, 46);
             this.NewNoteButton.TabIndex = 7;
@@ -133,7 +148,7 @@
             this.SaveButton.BackColor = System.Drawing.Color.White;
             this.SaveButton.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.SaveButton.ForeColor = System.Drawing.Color.LightSalmon;
-            this.SaveButton.Location = new System.Drawing.Point(481, 590);
+            this.SaveButton.Location = new System.Drawing.Point(493, 590);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(115, 46);
             this.SaveButton.TabIndex = 8;
@@ -141,19 +156,31 @@
             this.SaveButton.UseVisualStyleBackColor = false;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
+            // SavedNotes
+            // 
+            this.SavedNotes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SavedNotes.ForeColor = System.Drawing.Color.LightSalmon;
+            this.SavedNotes.Location = new System.Drawing.Point(12, 151);
+            this.SavedNotes.Name = "SavedNotes";
+            this.SavedNotes.Size = new System.Drawing.Size(463, 28);
+            this.SavedNotes.TabIndex = 9;
+            this.SavedNotes.Text = "Saved Notes";
+            this.SavedNotes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // frmNotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1253, 653);
+            this.Controls.Add(this.SavedNotes);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.NewNoteButton);
             this.Controls.Add(this.DeleteButton);
             this.Controls.Add(this.EditButton);
             this.Controls.Add(this.PreviousNotes);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Notes);
+            this.Controls.Add(this.Title);
             this.Controls.Add(this.NotesBox);
             this.Controls.Add(this.TitleBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -170,12 +197,13 @@
 
         private TextBox TitleBox;
         private TextBox NotesBox;
-        private Label label1;
-        private Label label2;
+        private Label Title;
+        private Label Notes;
         private DataGridView PreviousNotes;
         private Button EditButton;
         private Button DeleteButton;
         private Button NewNoteButton;
         private Button SaveButton;
+        private Label SavedNotes;
     }
 }
